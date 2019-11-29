09a8833f5fb71df904b2da3918498b719dc25895
James-Livesey <jamesliveg@gmail.com>
2019-11-29 09:01:49 +0000

    Add menu system to script

--- IMCUI/script.js
+++ IMCUI/script.js
@@ -54,6 +54,30 @@ function closeDialog() {
     $(".dialog").fadeOut();
 }
 
+function openMenu(menuName) {
+    $("[data-menu='" + menuName + "']").fadeIn();
+
+    $("[data-menu-dropdown='" + menuName + "']").attr("aria-expanded", "true");
+    $("[data-menu-dropdown-icon='" + menuName + "']").text("arrow_drop_up");
+
+    $("[data-menu='" + menuName + "']").focus();
+}
+
+function closeMenu(menuName) {
+    $("[data-menu='" + menuName + "']").fadeOut();
+
+    $("[data-menu-dropdown='" + menuName + "']").attr("aria-expanded", "false");
+    $("[data-menu-dropdown-icon='" + menuName + "']").text("arrow_drop_down");
+}
+
+function toggleMenu(menuName) {
+    if ($("[data-menu='" + menuName + "']").is(":visible")) {
+        closeMenu(menuName);
+    } else {
+        openMenu(menuName);
+    }
+}
+
 $(function() {
     setInterval(function() {
         loaderRotationAngle += 90;
@@ -64,4 +88,14 @@ $(function() {
             loaderRotationAngle = 0;
         }
     }, 150);
+    
+    $(window).click(function() {
+        $("[data-menu]").each(function() {
+            closeMenu($(this).attr("data-menu"));
+        });
+    });
+
+    $("[data-menu], [data-menu-dropdown]").click(function() {
+        event.stopPropagation();
+    });
 });
