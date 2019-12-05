0bbe0c4a6bd89c7130a753a714138baee3710a18
James-Livesey <jamesliveg@gmail.com>
2019-12-05 19:03:57 +0000

    Add lcApps library

---
+++ lcApps/script.js
@@ -0,0 +1,52 @@
+var currentUser = {
+    uid: null,
+    orgName: null,
+    org: null,
+    orgMember: null
+};
+
+function addProgramme(programme, programmeKey) {
+    $("<div class='programmeTile'>")
+        .text(programme.textShows != false ? programme.name : "")
+        .css({
+            "background-image": programme.thumbnail == null ? "url('https://imcnetwork.cf/LiveCloud/media/Blank%20App.png')" : "url('" + encodeURI(programme.thumbnail) + "')",
+            "background-repeat": "no-repeat",
+            "background-position": "center center",
+            "background-size": "cover",
+            "color": programme.textColour == null ? "var(--foregroundColour)" : programme.textColour
+        })
+        .appendTo(
+            $("<a class='programmeTileLink'>")
+                .attr({
+                    "title": programme.name,
+                    "href": window.location.href.split("?")[0] + "?prog=" + encodeURIComponent(programmeKey),
+                })
+                .appendTo($(".programmesList.myProgrammes"))
+        )
+    ;
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        currentUser.uid = user.uid;
+
+        firebase.database().ref("users/" + currentUser.uid + "/org").once("value", function(orgNameSnapshot) {
+            currentUser.orgName = orgNameSnapshot.val();
+
+            firebase.database().ref("orgs/" + currentUser.orgName).once("value", function(orgSnapshot) {
+                currentUser.org = orgSnapshot.val();
+                currentUser.orgMember = orgSnapshot.val().members[currentUser.uid];
+
+                firebase.database().ref("orgs/" + currentUser.orgName + "/programmes").on("value", function(snapshot) {
+                    $(".programmesList.myProgrammes").html("");
+
+                    for (var key in snapshot.val()) {
+                        addProgramme(snapshot.val()[key], key);
+                    }
+                });
+            });
+        });
+    } else {
+        window.location.href = "index.html";
+    }
+});
